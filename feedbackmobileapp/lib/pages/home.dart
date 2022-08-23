96b3194e53cbe91d1850044873186773b3de746e
kayuyu.mwaura <kayuyu.mwaura@EHR-03>
2022-08-23 17:38:47 +0300

    added rest of the fields

--- feedbackmobileapp/lib/pages/home.dart
+++ feedbackmobileapp/lib/pages/home.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:feedbackmobileapp/color/light_color.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -9,30 +11,88 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  double? _ratingValue;
+  double? _ratingValue1;
+  double? _ratingValue2;
+  double? _ratingValue3;
+  double? _ratingValue4;
+  double? _ratingValue5;
+
+  var listLocations = [
+    'Adult Casualty',
+    'Consulting Clinics',
+    'Lab',
+    'Paeds Casualty',
+    'Pharmacy',
+    'Radiology',
+    'Wards',
+    'Other'
+  ];
+  String selectedLocation = 'Adult Casualty';
+
+  void initState() {}
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
         child: Column(
-     children: [
-      Container(
-        margin: EdgeInsets.fromLTRB(10, 30, 10, 10),
+      children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+          padding: EdgeInsets.all(8.0),
+          // color: Colors.white,
+          // width: 370,
+          // height: 50,
+          child: Text('PLEASE FILL IN THIS FORM')
+        ),
+
+        Container(
+          margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
           padding: EdgeInsets.all(8.0),
           color: Colors.white,
           width: 370,
-          height: 180,
+          height: 300,
           child: Column(
             children: [
               TextFormField(
-                
                 decoration: InputDecoration(
                   labelText: 'Phone Number',
                   hintText: 'Please enter your phone number',
                 ),
-              )
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                  hintText: 'Please enter your name',
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Please enter your email address',
+                ),
+              ),
+              // DropdownButtonFormField(
+              //   isExpanded: true,
+              //   hint: Text('Select area of service'),
+              //   decoration: const InputDecoration(
+              //       contentPadding: const EdgeInsets.fromLTRB(0, 5.5, 0, 0),
+              //       labelStyle: TextStyle(),
+              //       //icon:  Icon(Icons.transgender),
+              //       labelText: 'Select area of service'),
+              //   value: listLocations,
+              //   items: listLocations.map((String listLocations) {
+              //     return DropdownMenuItem(
+              //         child: Text(listLocations), value: listLocations);
+              //   }).toList(),
+              //   onChanged: (newval) {
+              //     setState(() {
+              //       selectedLocation = newval.toString();
+              //     });
+              //   },
+              // ),
             ],
           ),
-      ),
+        ),
         Container(
           margin: EdgeInsets.all(10),
           padding: EdgeInsets.all(8.0),
@@ -41,41 +101,243 @@ class _HomePageState extends State<HomePage> {
           height: 180,
           child: Column(
             children: [
-              Text('Please rate the Lab service.'),
+              Text(
+                'How would you rate the experience?',
+                style: TextStyle(color: Colors.blueGrey[700], fontSize: 16),),
               SizedBox(height: 10),
-     
               RatingBar(
-                      initialRating: 0,
-                      direction: Axis.horizontal,
-                      allowHalfRating: true,
-                      itemCount: 5,
-                      ratingWidget: RatingWidget(
-                          full: const Icon(Icons.star, color: Colors.orange),
-                          half: const Icon(
-                            Icons.star_half,
-                            color: Colors.orange,
-                          ),
-                          empty: const Icon(
-                            Icons.star_outline,
-                            color: Colors.orange,
-                          )),
-                      onRatingUpdate: (value) {
-                        setState(() {
-                          _ratingValue = value;
-                        });
-                         SizedBox(height: 25);
-                  // Display the rate in number
-                  
-     }),
-                     Text(
-                      _ratingValue != null ? _ratingValue.toString() : 'Rate it!',
-                      style: const TextStyle(color: Colors.black, fontSize: 20),
-                    ),
+                  initialRating: 0,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  ratingWidget: RatingWidget(
+                      full: const Icon(Icons.star, color: Colors.orange),
+                      half: const Icon(
+                        Icons.star_half,
+                        color: Colors.orange,
+                      ),
+                      empty: const Icon(
+                        Icons.star_outline,
+                        color: Colors.orange,
+                      )),
+                  onRatingUpdate: (value) {
+                    setState(() {
+                      _ratingValue1 = value;
+                    });
+                    SizedBox(height: 25);
+                    // Display the rate in number
+                  }),
+              Text( //_ratingValue.toString(),
+                _ratingValue1 != null ? _ratingValue1.toString() : 'Rate it',
+                style: const TextStyle(color: Colors.blueGrey, fontSize: 16),
+              ),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Remark',
+                  hintText: 'Please enter your remarks',
+                ),)
             ],
           ),
         ),
-     
-     ],
+
+                Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(8.0),
+          color: Colors.white,
+          width: 370,
+          height: 180,
+          child: Column(
+            children: [
+              Text(
+                'How would you rate staff courtesy?',
+                style: TextStyle(color: Colors.blueGrey[700], fontSize: 16),),
+              SizedBox(height: 10),
+              RatingBar(
+                  initialRating: 0,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  ratingWidget: RatingWidget(
+                      full: const Icon(Icons.star, color: Colors.orange),
+                      half: const Icon(
+                        Icons.star_half,
+                        color: Colors.orange,
+                      ),
+                      empty: const Icon(
+                        Icons.star_outline,
+                        color: Colors.orange,
+                      )),
+                  onRatingUpdate: (value) {
+                    setState(() {
+                      _ratingValue2 = value;
+                    });
+                    SizedBox(height: 25);
+                    // Display the rate in number
+                  }),
+              Text( //_ratingValue.toString(),
+                _ratingValue2 != null ? _ratingValue2.toString() : 'Rate it',
+                style: const TextStyle(color: Colors.blueGrey, fontSize: 16),
+              ),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Remark',
+                  hintText: 'Please enter your remarks',
+                ),)
+            ],
+          ),
+        ),
+
+              Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(8.0),
+          color: Colors.white,
+          width: 370,
+          height: 180,
+          child: Column(
+            children: [
+              Text(
+                'How would you rate our failities?',
+                style: TextStyle(color: Colors.blueGrey[700], fontSize: 16),),
+              SizedBox(height: 10),
+              RatingBar(
+                  initialRating: 0,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  ratingWidget: RatingWidget(
+                      full: const Icon(Icons.star, color: Colors.orange),
+                      half: const Icon(
+                        Icons.star_half,
+                        color: Colors.orange,
+                      ),
+                      empty: const Icon(
+                        Icons.star_outline,
+                        color: Colors.orange,
+                      )),
+                  onRatingUpdate: (value) {
+                    setState(() {
+                      _ratingValue3 = value;
+                    });
+                    SizedBox(height: 25);
+                    // Display the rate in number
+                  }),
+              Text( //_ratingValue.toString(),
+                _ratingValue3 != null ? _ratingValue3.toString() : 'Rate it',
+                style: const TextStyle(color: Colors.blueGrey, fontSize: 16),
+              ),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Remark',
+                  hintText: 'Please enter your remarks',
+                ),)
+            ],
+          ),
+        ),
+
+              Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(8.0),
+          color: Colors.white,
+          width: 370,
+          height: 180,
+          child: Column(
+            children: [
+              Text(
+                'How would you rate our timeliness of service?',
+                style: TextStyle(color: Colors.blueGrey[700], fontSize: 16),),
+              SizedBox(height: 10),
+              RatingBar(
+                  initialRating: 0,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  ratingWidget: RatingWidget(
+                      full: const Icon(Icons.star, color: Colors.orange),
+                      half: const Icon(
+                        Icons.star_half,
+                        color: Colors.orange,
+                      ),
+                      empty: const Icon(
+                        Icons.star_outline,
+                        color: Colors.orange,
+                      )),
+                  onRatingUpdate: (value) {
+                    setState(() {
+                      _ratingValue4 = value;
+                    });
+                    SizedBox(height: 25);
+                    // Display the rate in number
+                  }),
+              Text( //_ratingValue.toString(),
+                _ratingValue4 != null ? _ratingValue4.toString() : 'Rate it',
+                style: const TextStyle(color: Colors.blueGrey, fontSize: 16),
+              ),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Remark',
+                  hintText: 'Please enter your remarks',
+                ),)
+            ],
+          ),
+        ),
+
+              Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(8.0),
+          color: Colors.white,
+          width: 370,
+          height: 180,
+          child: Column(
+            children: [
+              Text(
+                'How would you recommend us to others?',
+                style: TextStyle(color: Colors.blueGrey[700], fontSize: 16),),
+              SizedBox(height: 10),
+              RatingBar(
+                  initialRating: 0,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  ratingWidget: RatingWidget(
+                      full: const Icon(Icons.star, color: Colors.orange),
+                      half: const Icon(
+                        Icons.star_half,
+                        color: Colors.orange,
+                      ),
+                      empty: const Icon(
+                        Icons.star_outline,
+                        color: Colors.orange,
+                      )),
+                  onRatingUpdate: (value) {
+                    setState(() {
+                      _ratingValue5 = value;
+                    });
+                    SizedBox(height: 25);
+                    // Display the rate in number
+                  }),
+              Text( //_ratingValue.toString(),
+                _ratingValue5 != null ? _ratingValue5.toString() : 'Rate it',
+                style: const TextStyle(color: Colors.blueGrey, fontSize: 16),
+              ),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Remark',
+                  hintText: 'Please enter your remarks',
+                ),)
+            ],
+          ),
+        ),
+
+        Container(
+          
+        )
+
+      ],
     ));
   }
 }
